16ee66fa9b977f9680e872841a8523b80434c3f5
Eduardo Zaparoli <eduzzx1204@gmail.com>
2024-06-01 10:13:47 -0300

    implement getClient

--- frontend/src/service/APIService/index.ts
+++ frontend/src/service/APIService/index.ts
@@ -1,65 +1,78 @@
-import { AxiosInstance, RawAxiosRequestHeaders } from 'axios';
-import apiClientService from '../APIClientService';
-import * as types from './types';
+import { AxiosInstance, RawAxiosRequestHeaders } from "axios";
+import apiClientService from "../APIClientService";
+import * as types from "./types";
 
 class API {
-  constructor(private api: AxiosInstance) { }
+	constructor(private api: AxiosInstance) {}
 
-  public postUser = async (firstName: string, lastName: string, email: string, password: string): Promise<types.RegisterUser> => {
-    const { data } = await this.api.post<types.RegisterUser>('http://localhost:5000/user',
-      {
-        firstName,
-        lastName,
-        email,
-        password
-      },
-      {
-        headers: this.baseHeaders()
-      }
-    );
-    return data;
-  };
+	public getClient = async (documentNumber: string): Promise<types.Client> => {
+		const { data } = await this.api.get<types.Client>(`http://localhost:5000/user/documentNumber/${documentNumber}`, {
+			headers: this.baseHeaders(),
+		});
+		return data;
+	};
 
-  public resetPasswordUser = async (email: string, newPassword: string): Promise<types.ResetPassword> => {
-    const { data } = await this.api.patch<types.ResetPassword>('http://localhost:5000/user/reset-password',
-      {
-        email,
-        newPassword
-      },
-      {
-        headers: this.baseHeaders()
-      }
-    );
-    return data;
-  };
+	public postUser = async (
+		firstName: string,
+		lastName: string,
+		email: string,
+		password: string,
+	): Promise<types.RegisterUser> => {
+		const { data } = await this.api.post<types.RegisterUser>(
+			"http://localhost:5000/user",
+			{
+				firstName,
+				lastName,
+				email,
+				password,
+			},
+			{
+				headers: this.baseHeaders(),
+			},
+		);
+		return data;
+	};
 
-  public getAuthorizationToken = async (email: string, password: string): Promise<types.AccessToken> => {
-    const { data } = await this.api.post<types.AccessToken>('http://localhost:5000/login',
-      {
-        email,
-        password
-      },
-      {
-        headers: this.baseHeaders()
-      }
-    );
+	public resetPasswordUser = async (email: string, newPassword: string): Promise<types.ResetPassword> => {
+		const { data } = await this.api.patch<types.ResetPassword>(
+			"http://localhost:5000/user/reset-password",
+			{
+				email,
+				newPassword,
+			},
+			{
+				headers: this.baseHeaders(),
+			},
+		);
+		return data;
+	};
 
-    return data;
-  };
+	public getAuthorizationToken = async (email: string, password: string): Promise<types.AccessToken> => {
+		const { data } = await this.api.post<types.AccessToken>(
+			"http://localhost:5000/login",
+			{
+				email,
+				password,
+			},
+			{
+				headers: this.baseHeaders(),
+			},
+		);
 
-  private headerWithAuthentication = (token: string, headers?: RawAxiosRequestHeaders): RawAxiosRequestHeaders =>
-    this.baseHeaders({ Authorization: `Bearer ${token}`, ...headers });
+		return data;
+	};
 
+	private headerWithAuthentication = (token: string, headers?: RawAxiosRequestHeaders): RawAxiosRequestHeaders =>
+		this.baseHeaders({ Authorization: `Bearer ${token}`, ...headers });
 
-  private baseHeaders = (headers?: RawAxiosRequestHeaders): RawAxiosRequestHeaders => ({
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Accept": "application/json",
-    ...headers,
-  });
-
-};
+	private baseHeaders = (headers?: RawAxiosRequestHeaders): RawAxiosRequestHeaders => ({
+		"Content-Type": "application/json",
+		"Access-Control-Allow-Origin": "*",
+		"Accept": "application/json",
+		...headers,
+	});
+}
 
 export const api = new API(apiClientService);
 
-export { types as apiTypes }
+export { types as apiTypes };
