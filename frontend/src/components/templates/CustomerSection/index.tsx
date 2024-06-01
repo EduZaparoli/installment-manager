4b933e0bf03001e8c7a06ab3222bf3e8d010ba7e
Eduardo Zaparoli <eduzzx1204@gmail.com>
2024-06-01 10:14:02 -0300

    integration getClient

--- frontend/src/components/templates/CustomerSection/index.tsx
+++ frontend/src/components/templates/CustomerSection/index.tsx
@@ -1,25 +1,23 @@
-import {
-  CustomerCard,
-  CustomerCardProps,
-} from "@/components/molecules/CustomerCard";
+import { CustomerCard, CustomerCardProps } from "@/components/molecules/CustomerCard";
 import { Grid } from "@chakra-ui/react";
+import { observer } from "mobx-react-lite";
 
 export interface CustomerSectionProps {
-  customer: CustomerCardProps;
+	customer: CustomerCardProps;
 }
 
-export const CustomerSection = ({ customer }: CustomerSectionProps) => {
-  return (
-    <Grid
-      gridColumnGap={"24px"}
-      gridRowGap={"24px"}
-      maxW={"1440px"}
-      w={"full"}
-      p={"40px 108px"}
-      gap={"24px"}
-      margin={"0 auto"}
-    >
-      <CustomerCard {...customer} />
-    </Grid>
-  );
-};
+export const CustomerSection = observer(({ customer }: CustomerSectionProps) => {
+	return (
+		<Grid
+			gridColumnGap={"24px"}
+			gridRowGap={"24px"}
+			maxW={"1440px"}
+			w={"full"}
+			p={"40px 108px"}
+			gap={"24px"}
+			margin={"0 auto"}
+		>
+			<CustomerCard {...customer} />
+		</Grid>
+	);
+});
