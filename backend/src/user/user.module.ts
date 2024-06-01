e7296a525471c0b031065ecc793d3b30e1b0ae9f
Eduardo Zaparoli <eduzzx1204@gmail.com>
2024-06-01 10:09:35 -0300

    implement findByDocumentNumber function

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { Module } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserController } from "./user.controller";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { HttpModule } from "@nestjs/axios";
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [UserController],
-  providers: [UserService],
-  exports: [UserService],
+	imports: [
+		PrismaModule,
+		HttpModule.registerAsync({
+			useFactory: () => ({
+				timeout: 5000,
+				maxRedirects: 5,
+			}),
+		}),
+	],
+	controllers: [UserController],
+	providers: [UserService],
+	exports: [UserService],
 })
 export class UserModule {}
